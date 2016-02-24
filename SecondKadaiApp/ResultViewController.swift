6766b30bed7bb4aba9e99dee4f9373d4fe92de84
test_matsuot <matsuot@isao.co.jp>
2016-02-25 02:28:11 +0900

    課題再提出commit

---
+++ SecondKadaiApp/ResultViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ResultViewController.swift
+//  SecondKadaiApp
+//
+//  Created by m20 on 2016/02/24.
+//  Copyright © 2016年 takashi.matsuo. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    @IBOutlet weak var resultLabel: UILabel!
+    var name:String = ""
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        resultLabel.text = "こんにちは、\(name)さん"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textNameBox: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +20,16 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?){
+        // segueから遷移先のResultViewControllerを取得する
+        let resultViewController:ResultViewController = segue.destinationViewController as! ResultViewController
+        // 遷移先のResultViewControllerで宣言しているresultLabelにtext入力文字を渡す
+        resultViewController.name = textNameBox.text!
+    }
+    
+    @IBAction func unwind(segue: UIStoryboardSegue) {
+    }
 
 }
 
